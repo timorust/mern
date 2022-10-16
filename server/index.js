a6e0d7ce02e61ce25455f40f18a38aa356cc25b4
unknown <timorust1@gmail.comm>
2022-10-16 20:22:26 +0300

    first commit

---
+++ server/index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const UserModel = require('./models/Users')
+const cors = require('cors')
+
+app.use(express.json())
+app.use(cors())
+
+mongoose.connect(
+	'mongodb+srv://timor:memories123@cluster4.e5iysi3.mongodb.net/memories?retryWrites=true&w=majority'
+)
+
+app.get('/getUsers', (req, res) => {
+	UserModel.find({}, (err, result) => {
+		if (err) {
+			res.json(err)
+		} else {
+			res.json(result)
+		}
+	})
+})
+
+app.post('/createUser', async (req, res) => {
+	const user = req.body
+	const newUser = new UserModel(user)
+	await newUser.save()
+
+	res.json(user)
+})
+
+app.listen(3001, () => {
+	console.log('SERVER RUNS PERFECTLY!')
+})
